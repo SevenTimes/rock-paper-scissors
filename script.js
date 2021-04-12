50601e394407b9d199f6923cae6eb0889336002f
SevenTimes <krab47@yahoo.com>
2021-04-12 14:18:46 +0400

    working buttons

--- script.js
+++ script.js
@@ -1,42 +1,57 @@
 const choices = ['ROCK', 'PAPER', 'SCISSORS'];
-// let playerSelection = prompt('Choose: Rock, Paper or Scissors');
-// let computerSelection = computerPlay();
+const btns = Array.from(document.querySelectorAll('button'));
 
 function computerPlay() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
-  let playerChoice = playerSelection.toUpperCase();
-  if (playerChoice === computerSelection) {
-    return 'Tied';
-  } else if (playerChoice === 'ROCK') {
-    if (computerSelection === 'SCISSORS') {
-      return 'You win';
+btns.forEach((btn) =>
+  btn.addEventListener('click', () => {
+    let cpuChoice = computerPlay();
+    let playerChoice = btn.id;
+    if (playerChoice === cpuChoice) {
+      console.log('Tied');
+    } else if (playerChoice === 'ROCK') {
+      if (cpuChoice === 'SCISSORS') {
+        console.log('You Win');
+      } else {
+        console.log('You Lose');
+      }
+    } else if (playerChoice === 'PAPER') {
+      if (cpuChoice === 'ROCK') {
+        console.log('You Win');
+      } else {
+        console.log('You Lose');
+      }
+    } else if (playerChoice === 'SCISSORS') {
+      if (cpuChoice === 'PAPER') {
+        console.log('You Win');
+      } else {
+        console.log('You Lose');
+      }
     }
-    return 'You lose';
-  } else if (playerChoice === 'PAPER') {
-    if (computerSelection === 'ROCK') {
-      return 'You win';
-    }
-    return 'You lose';
-  } else if (playerChoice === 'SCISSORS') {
-    if (computerSelection === 'PAPER') {
-      return 'You win';
-    }
-    return 'You lose';
-  }
-  return 'Please insert correct choice';
-}
+  })
+);
 
-function game() {
-  for (i = 0; i < 5; i++) {
-    let playerSelection = prompt('Choose: Rock, Paper or Scissors');
-    let computerSelection = computerPlay();
-    let result = playRound(playerSelection, computerSelection);
-    console.log(result);
-  }
-}
-
-// console.log(playRound(playerSelection, computerSelection));
-game();
+// function playRound(playerSelection, cpuChoice) {
+//   let playerChoice = playerSelection.toUpperCase();
+//   if (playerChoice === cpuChoice) {
+//     return 'Tied';
+//   } else if (playerChoice === 'ROCK') {
+//     if (cpuChoice === 'SCISSORS') {
+//       return 'You win';
+//     }
+//     return 'You lose';
+//   } else if (playerChoice === 'PAPER') {
+//     if (cpuChoice === 'ROCK') {
+//       return 'You win';
+//     }
+//     return 'You lose';
+//   } else if (playerChoice === 'SCISSORS') {
+//     if (cpuChoice === 'PAPER') {
+//       return 'You win';
+//     }
+//     return 'You lose';
+//   }
+//   return 'Please insert correct choice';
+// }
